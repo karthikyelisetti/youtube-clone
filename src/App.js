be95a0d2350983b84fc8aaacec8b4d80677f3e85
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-06-27 09:39:21 +0530

    implemented Navbar and logo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+}
+
+export default App;
