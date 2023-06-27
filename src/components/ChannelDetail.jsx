5fd32129e3148ec1d8746a11054480d7ab0864f9
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-06-27 23:43:45 +0530

    WIP: implemented Videos page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState();
+
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{ background: "linear-gradient(to right, #b993d6, #8ca6db)", zIndex: 10, height: "300px" }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px"/>
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{mr: {sm: "100px"}}} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
